548d8636474c45411b74f35a337514567f5dd384
kate15k213 <104782071+kate15k213@users.noreply.github.com>
2024-11-12 01:34:57 +0900

    Add files via upload

--- js/botton.js
+++ js/botton.js
@@ -1,18 +1,18 @@
-$(function() {
+$(function () {
   // 変数にクラスを入れる
   var btn = $('.button');
 
   //スクロールしてページトップから100に達したらボタンを表示
-  $(window).on('load scroll', function(){
-    if($(this).scrollTop() > 800) {
+  $(window).on('load scroll', function () {
+    if ($(this).scrollTop() > 800) {
       btn.addClass('bottonactive');
-    }else{
+    } else {
       btn.removeClass('bottonactive');
     }
   });
 
   //スクロールしてトップへ戻る
-  btn.on('click',function () {
+  btn.on('click', function () {
     $('body,html').animate({
       scrollTop: 0
     });
--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,26 @@
-$(function () {
-
-  var element = document.querySelector('.header-box');
-  const text = document.querySelectorAll('.nava');
-  //スクロールしてページトップから100に達したlogo.文字、背景を変更して固定する。
-  $(window).on('load scroll', function () {
-    if ($(this).scrollTop() > 900) {
-      element.style.backgroundColor = '#fff';
-      element.style.position = 'fixed';
-      $('.header-logo').css({
-        backgroundImage: $('.header-logo').css('background-image').replace('download-2.svg', 'download-16.svg')
-      });
-      for (let i = 0; i < text.length; i++) {
-        text[i].style.color = '#000';
-      }
-    } else {
-      element.style.backgroundColor = 'transparent';
-      element.style.position = 'static';
-      $('.header-logo').css({
-        backgroundImage: $('.header-logo').css('background-image').replace('download-16.svg', 'download-2.svg')
-      });
-      for (let i = 0; i < text.length; i++) {
-        text[i].style.color = '#fff';
-      }
-    }
-  });
-
-});
+// $(function () {
+//   var element = document.querySelector('.header-box');
+//   const text = document.querySelectorAll('.nava');
+//   //スクロールしてページトップから100に達したlogo.文字、背景を変更して固定する。
+//   $(window).on('load scroll', function () {
+//     if ($(this).scrollTop() > 900) {
+//       element.style.backgroundColor = '#fff';
+//       element.style.position = 'fixed';
+//       $('.header-logo').css({
+//         backgroundImage: $('.header-logo').css('background-image').replace('download-2.svg', 'download-16.svg')
+//       });
+//       for (let i = 0; i < text.length; i++) {
+//         text[i].style.color = '#000';
+//       }
+//     } else {
+//       element.style.backgroundColor = 'transparent';
+//       element.style.position = 'static';
+//       $('.header-logo').css({
+//         backgroundImage: $('.header-logo').css('background-image').replace('download-16.svg', 'download-2.svg')
+//       });
+//       for (let i = 0; i < text.length; i++) {
+//         text[i].style.color = '#fff';
+//       }
+//     }
+//   });
+// });
