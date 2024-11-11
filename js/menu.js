e77e477cd6bf5ecedbe65d19ad61fa307210f528
kate15k213 <104782071+kate15k213@users.noreply.github.com>
2024-11-12 00:42:11 +0900

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,27 @@
-$(function() {
-  // 要素を取得
-  var element = document.querySelector('.menu');
-  //スクロールしてページトップから100に達したらボタンを表示
-  $(window).on('load scroll', function(){
-    if($(this).scrollTop() > 900) {
-      element.style.display='block';
-    }else{
-      element.style.display='none';
+$(function () {
+
+  var element = document.querySelector('.header-box');
+  const text = document.querySelectorAll('.nava');
+  //スクロールしてページトップから100に達したlogo.文字、背景を変更して固定する。
+  $(window).on('load scroll', function () {
+    if ($(this).scrollTop() > 900) {
+      element.style.backgroundColor = '#fff';
+      element.style.position = 'fixed';
+      $('.header-logo').css({
+        backgroundImage: $('.header-logo').css('background-image').replace('download-2.svg', 'download-16.svg')
+      });
+      for (let i = 0; i < text.length; i++) {
+        text[i].style.color = '#000';
+      }
+    } else {
+      element.style.backgroundColor = 'transparent';
+      element.style.position = 'static';
+      $('.header-logo').css({
+        backgroundImage: $('.header-logo').css('background-image').replace('download-16.svg', 'download-2.svg')
+      });
+      for (let i = 0; i < text.length; i++) {
+        text[i].style.color = '#fff';
+      }
     }
   });
 
